1b1e2a72c514eb339f9601e28e4c92818d9afeae
stong <stong1108@gmail.com>
2017-01-10 10:30:59 -0800

    initial commit

---
+++ day1.py
@@ -0,0 +1,29 @@
+class BlockCounter(object):
+
+    def __init__(self):
+        self.loc = (0, 0)
+        self.commands = None
+        self.dir_len = [0, 0, 0, 0] # [north, east, south, west]
+        self.curr_dir = 0
+
+    def read_input(self, string):
+        self.commands = string.split(', ')
+
+    def one_command(self, command):
+        command_dir = command[:1]
+        command_len = int(command[1:])
+        if command_dir == 'R':
+            self.curr_dir = (self.curr_dir + 1) % 4
+        elif command_dir == 'L':
+            self.curr_dir = (self.curr_dir - 1) % 4
+        self.dir_len[self.curr_dir] += command_len
+
+    def follow_commands(self):
+        for cmd in self.commands:
+            self.one_command(cmd)
+        return abs(self.dir_len[0] - self.dir_len[2]) + abs(self.dir_len[1] - self.dir_len[3])
+
+if __name__ == "__main__":
+    bc = BlockCounter()
+    bc.read_input(raw_input('Insert input: '))
+    print bc.follow_commands()
