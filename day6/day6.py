79683a6e1545f040ad3809769afbaf135f4e191e
stong <stong1108@gmail.com>
2017-01-20 07:46:22 -0600

    finish day6

--- day6/day6.py
+++ day6/day6.py
@@ -3,7 +3,8 @@ class Solution(object):
     def __init__(self):
         self.lines = []
         self.transposed = None
-        self.message = ''
+        self.most_message = ''
+        self.least_message = ''
 
     def read_input(self, filename):
         f = open(filename, 'r')
@@ -11,18 +12,23 @@ class Solution(object):
             self.lines.append(list(ln.strip()))
         self.transposed = map(list, zip(*self.lines))
 
-    def _mostcommonletter(self, lst):
+    def _most_and_least_letter(self, lst):
         d = dict()
         for char in lst:
             d[char] = d.get(char, 0) + 1
-        return d.keys()[d.values().index(max(d.values()))]
+        most = d.keys()[d.values().index(max(d.values()))]
+        least = d.keys()[d.values().index(min(d.values()))]
+        return most, least
 
-    def get_message(self):
+    def get_messages(self):
         for lst in self.transposed:
-            self.message += self._mostcommonletter(lst)
-        return self.message
+            most, least = self._most_and_least_letter(lst)
+            self.most_message += most
+            self.least_message += least
 
 if __name__ == '__main__':
     s = Solution()
     s.read_input('day6.txt')
-    print s.get_message()
+    s.get_messages()
+    print 'Message 1: ' + s.most_message
+    print 'Message 2: ' + s.least_message
